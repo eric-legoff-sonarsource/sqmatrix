d6a1d46132ff0f9c5ff4faec3d9d4e8071c52403
Eric Le Goff <eric.legoff@sonarsource.com>
2023-02-01 15:17:07 +0100

    move migrations functions to model

--- app/main/migrations/0001_initial.py
+++ app/main/migrations/0001_initial.py
@@ -2,10 +2,8 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-import requests
-import re
-import base64
-import os
+from ..models import LANGUAGES, get_gradle_config, jar_list
+
 
 """SonarQube versions """
 SQ_VERSIONS=[
@@ -66,77 +64,8 @@ SQ_VERSIONS=[
 '8.8.0.42792',    
     ]
 
-LANGUAGES = {
-    "sonar-abap" : "ABAP",
-    "sonar-css" : "CSS",
-    "sonar-text" : "Text",
-    "sonar-json" : "JSON",
-    "sonar-xml" : "XML",
-    "sonar-python" : "Python",
-    "sonar-apex" : "Apex",
-    "sonar-html" : "HTML",
-    "sonar-tsql" : "TSQL",
-    "sonar-vb"  : "VB",
-    "sonar-iac" : "IaC",
-    "sonar-scala" : "Scala",
-    "sonar-go" : "Go",
-    "sonar-php" : "PHP",
-    "sonar-javascript" : "Javascript",
-    "sonar-java" : "Java",
-    "sonar-flex" : "Flex",
-    "sonar-vbnet" : "VbNet",
-    "sonar-csharp" : "C#",
-    "sonar-swift" : "Swift",
-    "sonar-rpg" : "RPG",
-    "sonar-pli" : "PL1",
-    "sonar-cfamily" : "C",
-    "sonar-cobol" : "COBOL"
-    
-    
-}
 
-def get_gradle_config(sqversion):
-    '''Provides the raw text content of the build.gradle file 
-    of the SonarQube version set as parameter'''
-    
-    token = os.getenv('SQMATRIX_TOKEN')
-    owner = 'SonarSource'
-    repo = 'sonar-enterprise'
-    path = 'build.gradle?ref={}'.format(sqversion)
 
-    r = requests.get(
-    'https://api.github.com/repos/{owner}/{repo}/contents/{path}'.format(
-    owner=owner, repo=repo, path=path),
-    headers={
-        'Accept': 'application/vnd.github+json',
-        'Authorization': 'Bearer {}'.format(token),
-        'X-GitHub-Api-Version': '2022-11-28'
-            }
-    )
-    data = r.json()
-    file_content = data.get('content', '')
-    file_content_encoding = data.get('encoding')
-    if file_content_encoding == 'base64':
-        file_content = base64.b64decode(file_content).decode()
-    
-    return file_content
-
-def jar_list(filecontent):
-    ''' Returns the list of Sonarsource jar files 
-    in the given gradle file'''
-    
-    result = []
-    lines =  filecontent.split('\n')
-    
-    regexp = '^.*dependency\s+\'(com|org)\.sonar.*?\:(.*[0-9]+)\'$'
-    
-    
-    for line in lines:
-        res = re.search(regexp, line)
-        if res:
-            if len(res.groups()) == 2:
-                result.append(res.group(2).split(':'))
-    return result  
 
 
 
