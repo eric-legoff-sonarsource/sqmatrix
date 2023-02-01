9c1e6764b60580e949f5c3be2cee000cc1a6540e
Eric Le Goff <eric.legoff@sonarsource.com>
2023-02-01 10:23:09 +0100

    sort versions alpha

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,6 @@ from .models import Sonarqube
 
 
 def index(request):
-    sqversions = Sonarqube.objects.all()
+    sqversions = Sonarqube.objects.all().order_by('-full_version')
     context = {'sqversions' : sqversions}
     return render(request, 'index.html', context)
