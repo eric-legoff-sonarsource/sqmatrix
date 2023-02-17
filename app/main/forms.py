c25a538e672ad4eacd474bf8a67f8115faff93dd
Eric Le Goff <eric.legoff@sonarsource.com>
2023-02-17 15:29:35 +0100

    jira tickets between releases

--- app/main/forms.py
+++ app/main/forms.py
@@ -19,7 +19,7 @@ class CompareReleases(forms.Form):
     CHOICES.append((s.full_version,s.full_version))
   
   v1 = forms.CharField(label="SonarQube from version", max_length=150,
-                           widget= forms.Select(choices=CHOICES))
+                           widget= forms.Select(choices=CHOICES[1:]))
   v2 = forms.CharField(label="SonarQube to version", max_length=150,
                            widget= forms.Select(choices=CHOICES)) 
   
