8f8420f834110ef4be1ebe4ed1f3a3b38380fda2
Eric Le Goff <eric.legoff@sonarsource.com>
2023-02-17 14:13:11 +0100

    Wip in comparison dropdowns

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from .models import Sonarqube
+
+
 
 
 class AddNewRelease(forms.Form):
+  
      # Label is the text that shows before the input-box
    full_version = forms.CharField(label="SonarQube version", max_length=150,
                            widget= forms.TextInput
@@ -9,9 +13,15 @@ class AddNewRelease(forms.Form):
    
    
 class CompareReleases(forms.Form):
+  sqversions = Sonarqube.objects.all().order_by('-full_version')
+  CHOICES = []
+  for s in sqversions:
+    CHOICES.append((s.full_version,s.full_version))
+  
   v1 = forms.CharField(label="SonarQube from version", max_length=150,
-                           widget= forms.TextInput
-                           (attrs={'placeholder':'from version number'}))
+                           widget= forms.Select(choices=CHOICES))
   v2 = forms.CharField(label="SonarQube to version", max_length=150,
-                           widget= forms.TextInput
-                           (attrs={'placeholder':'to version number'})) 
+                           widget= forms.Select(choices=CHOICES)) 
+  
+
+
